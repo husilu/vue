fcbe0a0159809301ae7af61635e7db3840c84f51
husilu <714429363@qq.com>
2021-01-03 23:19:12 +0800

    docs(源码解析): 源码解析

--- src/core/index.js
+++ src/core/index.js
@@ -2,7 +2,7 @@ import Vue from './instance/index'
 import { initGlobalAPI } from './global-api/index'
 import { isServerRendering } from 'core/util/env'
 import { FunctionalRenderContext } from 'core/vdom/create-functional-component'
-
+// 给Vue挂载静态方法
 initGlobalAPI(Vue)
 
 Object.defineProperty(Vue.prototype, '$isServer', {
@@ -10,7 +10,7 @@ Object.defineProperty(Vue.prototype, '$isServer', {
 })
 
 Object.defineProperty(Vue.prototype, '$ssrContext', {
-  get () {
+  get() {
     /* istanbul ignore next */
     return this.$vnode && this.$vnode.ssrContext
   }
